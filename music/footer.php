8e37d0edef6df99d70f6a628c36ea3b32a90b6bd
Nguyễn Tiến Minh <158137147+139148nguyen@users.noreply.github.com>
2024-06-23 23:33:37 +0700

    Sixth Commit

---
+++ music/footer.php
@@ -0,0 +1,7 @@
+</div>
+    <div class="footer">
+        <div class="advertise"></div>
+    </div>
+</body>
+
+</html>
---
+++ music/header.php
@@ -0,0 +1,67 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="./styles/base.css">
+    <link rel="stylesheet" href="./styles/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="body">
+        <div class="side-bar">
+            <div class="side-bar__top frame">
+                <ul class="nav-list">
+                    <i>
+                        <a href="" class="light-text label-text nav-item">
+                            <div class="icon"><i class="fa-solid fa-music"></i></div>
+                            <div class="label">Music</div>
+                        </a>
+                    </i>
+                    <i>
+                        <a href="" class="light-text label-text nav-item">
+                            <div class="icon"><i class="fa-solid fa-house"></i></div>
+                            <div class="label">Home</div>
+                        </a>
+                    </i>
+                    <i>
+                        <a href="" class="base-text label-text nav-item">
+                            <div class="icon"><i class="fa-solid fa-magnifying-glass"></i></div>
+                            <div class="label">Search</div>
+                        </a>
+                    </i>
+                </ul>
+            </div>
+            <div class="side-bar__bottom frame">
+                <ul class="nav-list">
+                    <i style="display: flex; align-items: center;">
+                        <a href="" class="nav-item base-text label-text" style="flex:5">
+                            <div class="icon"><i class="fa-solid fa-book"></i></div>
+                            <div class="label">Your Library</div>
+
+                        </a>
+                        <a class="icon-plus"><i class="fa-solid fa-plus"></i></a>
+                    </i>
+                    <i>
+                        <div class="box-library">
+
+                        </div>
+                    </i>
+                    <i>
+                        <div></div>
+                    </i>
+                    <i>
+                        <div>
+                            <a class="button label-text">
+                                <i class="fa-solid fa-globe"></i>
+                                English
+                            </a>
+                        </div>
+                    </i>
+                </ul>
+
+            </div>
+        </div>
+        
+    
